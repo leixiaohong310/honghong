5e1b5eaae76020d4e4803b0a38a7abce5af75ef9
leich <453180262@qq.com>
2024-03-08 18:24:42 +0800

    消息发不出去文章！

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,74 +1,75 @@
 module.exports = {
-  lang: 'zh-CN',
-  title: 'ChatGPT中文教程',
-  description: 'ChatGPT中文教程',
-  theme: 'vuepress-theme-vdoing',
-  head: [
-      ["script",
-         {},
-         `var _hmt = _hmt || [];
-         (function() {
-           var hm = document.createElement("script");
-           hm.src = "https://hm.baidu.com/hm.js?59033874dd991ce9246775fed052092a";
-           var s = document.getElementsByTagName("script")[0];
-           s.parentNode.insertBefore(hm, s);
-         })();`
-      ],
-      ["meta",{name: "keywords",content: "ChatGPT,ChatGPT Plus,GPT-4,GPT4,ChatGPT升级,ChatGPT账号,GPT-4升级,GPT4升级,ChatGPT中文教程,Onlyfans订阅"}]
-//      ['meta',{name: 'referrer', content: 'no-referrer-when-downgrade'}],
-//      ['link', {rel: 'stylesheet', href: 'https://at.alicdn.com/t/font_3077305_pt8umhrn4k9.css'}]
-
-      ],
-  plugins: {
-      'sitemap': {
-        hostname: 'https://aliyuntm.com'
-      }
-  },
-  themeConfig: {
-    logo: "/gptNew.png",
-    siteTitle: false,
-    nav: [
-      {
-        text: '首页',
-        link: '/'
-      },
-      {
-       text: 'ChatGPT',
-       link: '/chatgpt/'
-       },
-       {
-         text: 'ChatGPT PLUS',
-         link: '/chatgptplus/'
-       },
-       {
-         text: 'ChatGPT账号',
-         link: '/chatgpt-account/'
-      },
-      {
-       text: 'ChatGPT代充',
-       link: '/chatgpt-recharge/'
-      },
-      {
-      text: 'OnlyFans',
-      link: '/onlyfans/'
-       },
-      {
-        text: 'Sora',
-        link: '/sora/'
-       },
-      {
-       text: 'ChatGPT简介',
-       link: '/chatgpt-info/'
-      }
-    ],
-      sidebar: {
-          '/chatgpt/': ['how-to-update-gpt4'],
-          '/chatgpt-account/':['/'],
-          '/chatgpt-recharge/':['/'],
-          '/chatgpt-info/': ['what-is-chatgpt','AI-life','what-can-chatgpt-do','Asking-ChatGPT-for-High-Quality-Answers'],
-          '/chatgptplus/': ['upgrade-gtp','how-to-cancel-chatgptplus'],
-          '/onlyfans/': ['/'],
-          '/sora/': ['/']
-        }
-  }
+	lang: 'zh-CN',
+	title: 'ChatGPT中文教程',
+	description: 'ChatGPT中文教程',
+	theme: 'vuepress-theme-vdoing',
+	head: [["script", {},
+	`
+	var _hmt = _hmt || []; (function() {
+		var hm = document.createElement("script");
+		hm.src = "https://hm.baidu.com/hm.js?59033874dd991ce9246775fed052092a";
+		var s = document.getElementsByTagName("script")[0];
+		s.parentNode.insertBefore(hm, s);
+	})();`], ["meta", {
+		name: "keywords",
+		content: "ChatGPT,ChatGPT Plus,GPT-4,GPT4,ChatGPT升级,ChatGPT账号,GPT-4升级,GPT4升级,ChatGPT中文教程,Onlyfans订阅"
+	}]
+	//      ['meta',{name: 'referrer', content: 'no-referrer-when-downgrade'}],
+	//      ['link', {rel: 'stylesheet', href: 'https://at.alicdn.com/t/font_3077305_pt8umhrn4k9.css'}]
+	],
+	plugins: {
+		'sitemap': {
+			hostname: 'https://aliyuntm.com'
+		}
+	},
+	themeConfig: {
+		logo: "/gptNew.png",
+		siteTitle: false,
+		nav: [{
+			text: '首页',
+			link: '/'
+		},
+		{
+			text: 'ChatGPT',
+			link: '/chatgpt/'
+		},
+		{
+			text: 'ChatGPT PLUS',
+			link: '/chatgptplus/'
+		},
+		{
+			text: 'ChatGPT账号',
+			link: '/chatgpt-account/'
+		},
+		{
+			text: 'ChatGPT代充',
+			link: '/chatgpt-recharge/'
+		},
+		{
+			text: 'OnlyFans',
+			link: '/onlyfans/'
+		},
+		{
+			text: 'Sora',
+			link: '/sora/'
+		},
+		{
+			text: 'ChatGPT简介',
+			link: '/chatgpt-info/'
+		},
+		{
+			text: 'ChatGPT帮助中心',
+			link: '/chatgpt-help/'
+		}],
+		sidebar: {
+			'/chatgpt/': ['how-to-update-gpt4'],
+			'/chatgpt-account/': ['/'],
+			'/chatgpt-recharge/': ['/'],
+			'/chatgpt-info/': ['what-is-chatgpt', 'AI-life', 'what-can-chatgpt-do', 'Asking-ChatGPT-for-High-Quality-Answers'],
+			'/chatgptplus/': ['upgrade-gtp', 'how-to-cancel-chatgptplus'],
+			'/onlyfans/': ['/'],
+			'/sora/': ['/'],
+			'/chatgpt-help/': ['unable-to-send-messages']
+		}
+	}
 }
---
+++ docs/chatgpt-help/unable-to-send-messages.md
@@ -0,0 +1,37 @@
+---
+title: ChatGpt突然发不出去消息了？这么解决试试..
+description: ChatGPT账号,ChatGpt被禁用,ChatGpt消息发不出去
+meta: 
+  - 
+    name: description
+    content: ChatGPT账号,ChatGpt被禁用,ChatGpt消息发不出去
+  - 
+    name: keywords
+    content: ChatGPT账号,ChatGpt被禁用,ChatGpt消息发不出去
+tags: 
+  - ChatGPT账号,ChatGpt被禁用,ChatGpt消息发不出去
+permalink: /chatgpt-help/unable-to-send-messages.html
+categories: 
+  - chatgpt-help
+date: 2024-03-08 18:05:12
+---
+
+# ChatGpt突然发不出去消息了？这么解决试试..
+## 现象原因
+ChatGpt突然发不出去消息了，历史对话消息是可以看到的，但是新的问题消息发不出去...
+
+如果你的网络没问题，那么猜测可能是下面的原因
+
+应该都是点击登录后顶部弹窗邀请[加入多语言 alapha 测试]了，并且语言选择了中文，抓包看到ab.chatgpt.com接口有问题，控制台报错，拿不到 account信息。
+
+## 解决方案
+
+**临时解决方案**：浏览器打开无痕隐私模式打开ChatGpt
+
+**永久解决方案**：清理掉ChatGpt本地缓存，等于还原到最新，清空 https://chat.openai.com/的浏览器本地所有缓存数据。
+
+流程:ChatGPT对话页面-鼠标右键-检查(打开 devtools面板)-应用(顶部)-左侧应用下的存储-勾选「包括第三方cookies」-点击「清除网站数据」
+
+
+
+
